e2b95f640953dc0bad2777bb36ad5f2a6f1c4450
Matuesz Gruszczynski <mateusz.gruszczynski@azimo.com>
2018-04-27 15:03:39 +0200

    aaa

--- locustfile.py
+++ locustfile.py
@@ -1,17 +1,14 @@
 from locust import HttpLocust, TaskSet, task
 
 class WebsiteTasks(TaskSet):
-    def on_start(self):
-        self.client.get("/")
-    
+   
     @task
     def index(self):
-        self.client.get("/")
-        
+        r = self.client.get("/")
 
 class WebsiteUser(HttpLocust):
     task_set = WebsiteTasks
-    #host = "http://34.246.0.51:8081"
-    host = "http://softwareqa.pl"
+    host = "http://34.246.0.51:8081"
+    #host = "http://softwareqa.pl/dupa"
     min_wait = 5
     max_wait = 5
