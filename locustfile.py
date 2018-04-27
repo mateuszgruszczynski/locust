1d128c59916dc29c53730964ab06850180ddc934
Matuesz Gruszczynski <mateusz.gruszczynski@azimo.com>
2018-04-27 16:06:41 +0200

    inc req

--- locustfile.py
+++ locustfile.py
@@ -4,11 +4,14 @@ class WebsiteTasks(TaskSet):
    
     @task
     def index(self):
-        r = self.client.get("/")
+        self.client.get("/")
+        self.client.get("/")
+        self.client.get("/")
+        self.client.get("/")
+        self.client.get("/")
 
 class WebsiteUser(HttpLocust):
     task_set = WebsiteTasks
     host = "http://34.246.0.51:8081"
-    #host = "http://softwareqa.pl/dupa"
     min_wait = 5
     max_wait = 5
